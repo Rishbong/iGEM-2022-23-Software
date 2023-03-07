132221c4703bb0a571d463a36929842a568a1e05
Rishabh <r.das@cityoflondonschool.org.uk>
2023-03-07 20:11:05 +0000

    added functions to fimd complimentary strand and reverse a string specified the miRNA for the gen3 toehold switch made for endometriosis

--- AND Gate and Linker Sequence Generator/AND_Gate_and_Linker_Sequence_Generator.py
+++ AND Gate and Linker Sequence Generator/AND_Gate_and_Linker_Sequence_Generator.py
@@ -1,1 +1,23 @@
+#miRNA in question for gen3 switch
+HSA_miR_145_5p = "GUCCAGUUUUCCCAGGAAUCCCU" 
+HSA_miR_199a = "ACAGUAGUCUGCACAUUGGUUA"
 
+#dictionary defining complimentary base pairing (no wobble pairing present)
+baseDict = {"A":"U", "C":"G"}
+
+#computes complimentary strand 
+def compStrand(rnaStrand):
+    compliment = ""
+    for i in rnaStrand:
+        for key, value in baseDict.items():
+            if i == key:
+                compliment += value
+            elif i == value:
+                compliment += key
+    return compliment
+
+#reverses string
+def reverse(rnaStrand):
+  return rnaStrand[::-1]
+
+print(compStrand(HSA_miR_145_5p))
